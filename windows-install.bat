8615676531aa4a6324e4d122cb701bce4a9b58f5
Teddy Sudol <magesolumin@gmail.com>
2013-09-01 14:30:34 -0400

    Added the Windows installation script.
    It's a really simple script for now. I don't think it's buggy, but I
    could probably make it more robust by e.g. checking if .vimrc and
    vimfiles dir already exist in %USERPROFILE%. It's fine for now.
    
    TODO: Linux installation script.

---
+++ windows-install.bat
@@ -0,0 +1,48 @@
+@echo off
+
+:: Check if we have administrative rights.
+:: Modified from http://stackoverflow.com/a/11995662
+echo Administrative permissions required. Detecting permissions...
+echo.
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Administrative permissions confirmed.
+    echo.
+) else (
+    echo Failure: Current permissions inadequate.
+    echo Please run this .bat as administrator.
+    echo.
+    pause
+    exit
+)
+
+:: The vimfiles directory and the .vimrc file go in the same place:
+:: C:\Users\<user>, or the USERPROFILE directory.
+:: see :help runtimepath in vim for more information
+set vftarget="%USERPROFILE%\vimfiles\"
+set vrctarget="%USERPROFILE%\.vimrc"
+
+:: The %~d0%~p0 sequence gives us the current directory of the batch file.
+:: The ending '\' separator is already included.
+set vfsource="%~d0%~p0"
+set vrcsource="%~d0%~p0.vimrc"
+
+:: Let the user can see what's going on.
+echo Vimfiles directory:
+echo Source: %vfsource%
+echo Target: %vftarget%
+echo. 
+
+echo .vimrc:
+echo Source: %vrcsource%
+echo Target: %vrctarget%
+echo.
+
+:: The mklink command creates junctions, which are essentially symlinks.
+:: The /j option is for directory junctions.
+echo Creating junctions...
+mklink /j %vftarget% %vfsource%
+mklink %vrctarget% %vrcsource%
+
+pause
